dd5bf9b466b46e6b4211937b223a8ef14ee074ff
hunganh <scramblegf@gmail.com>
2020-03-31 23:06:08 +0300

    Updates

---
+++ precache-manifest.ef7ac7dc8d107a14067f3fd9ca6703d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed33cc06f4d67f94aa5ebbd3ebdb4ba5",
+    "url": "/randomjokes/index.html"
+  },
+  {
+    "revision": "10c81f8302e2a05d98cb",
+    "url": "/randomjokes/static/css/main.7295e8e9.chunk.css"
+  },
+  {
+    "revision": "e257fcbae9797afa7d2c",
+    "url": "/randomjokes/static/js/2.4c53ffdd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/randomjokes/static/js/2.4c53ffdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c81f8302e2a05d98cb",
+    "url": "/randomjokes/static/js/main.6ba10d8c.chunk.js"
+  },
+  {
+    "revision": "f872c6cd560c36ae8be6",
+    "url": "/randomjokes/static/js/runtime-main.d3f4139c.js"
+  }
+]);
